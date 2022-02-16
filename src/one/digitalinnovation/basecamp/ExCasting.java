0b23192f01d1e90d424ff7a13b8456ca5d111fca
StefanOliveira <carlos.stefan86@yahoo.com.br>
2022-02-16 16:28:08 -0300

    Casting

---
+++ src/one/digitalinnovation/basecamp/ExCasting.java
@@ -0,0 +1,17 @@
+package one.digitalinnovation.basecamp;
+
+/*
+    É a transformação de uma determinada variável de tipo menos
+    específico para um tipo mais específico ou vice-versa."
+    -Upcast(Implicito)
+    -Downcast(Explicito)
+    ex. simples:
+    long l; int i - 10; I = i; (Upcast)
+    float f; double d = 10.5d; f = (float) d; (Downcast)
+ */
+
+public class ExCasting {
+
+
+
+}
